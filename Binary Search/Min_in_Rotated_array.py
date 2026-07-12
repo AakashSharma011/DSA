12c58d57736e3f20d3e2de164718906ff1332d24
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-12 12:52:56 +0530

    More improvised code

--- Binary Search/Min_in_Rotated_array.py
+++ Binary Search/Min_in_Rotated_array.py
@@ -1,16 +1,14 @@
-def findMin(self, nums):
-        low=0
-        high=len(nums)-1
-        if nums[0]<=nums[-1]:
-            return nums[0]
-        
-        while low<=high:
-            mid =low+(high-low)//2
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]   # Pivot mil gaya
+class Solution(object):
+    def findMin(self, nums):
+        low = 0
+        high = len(nums) - 1
 
-            elif nums[mid] >= nums[low]:
-                low = mid + 1          # Left half sorted hai, pivot right me
+        while low < high:
+            mid = low + (high - low) // 2
 
+            if nums[mid] > nums[high]:
+                low = mid + 1
             else:
-                high = mid - 1
+                high = mid
+
+        return nums[low]
