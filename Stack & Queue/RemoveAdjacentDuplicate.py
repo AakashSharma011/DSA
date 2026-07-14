52b165841b132704713b9a50d8f60a4eacb378e2
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-14 18:11:39 +0530

    Remove Adjacent Duplicates from a String

---
+++ Stack & Queue/RemoveAdjacentDuplicate.py
@@ -0,0 +1,10 @@
+s='abbaca'
+def remove_adjacent_duplicates(s):
+    stack=[]
+    for ch in s:
+        if stack and stack[-1]==ch:
+            stack.pop()
+        else:
+            stack.append(ch)
+    return ''.join(stack)
+print(remove_adjacent_duplicates(s))
