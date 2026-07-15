26784cae8c78bd59325a7b384cc241a32a89677f
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-15 10:10:27 +0530

    Next Smaller

---
+++ Stack & Queue/NExt_smaller.py
@@ -0,0 +1,12 @@
+arr=[4,1,2,5,3]
+stack=[]
+ans=[]
+for i in arr[::-1]:
+    while stack and stack[-1]>=i:
+        stack.pop()
+    if stack:
+        ans.append(stack[-1])
+    else:
+        ans.append(-1)
+    stack.append(i)
+print(ans[::-1])
