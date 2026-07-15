a58ceffbf8a18d55dfd00342592e33b212158ad4
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-15 10:11:52 +0530

    Updated Previous smaller code

--- Stack & Queue/previous_smaller.py
+++ Stack & Queue/previous_smaller.py
@@ -4,9 +4,9 @@ ans=[]
 for i in arr:
     while stack and stack[-1]>=i:
         stack.pop()
-        if stack:
-            ans.append(stack[-1])
-        else:
-            ans.append(-1)
+    if stack:
+        ans.append(stack[-1])
+    else:
+        ans.append(-1)
     stack.append(i)
 print(ans)
