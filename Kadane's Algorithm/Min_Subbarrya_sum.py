136437cfdb647678359c5172167b160f5b36b04d
Aakash Sharma <11128rpvvgp@gmail.com>
2026-07-18 08:45:47 +0530

    Min subarray sum

---
+++ Kadane's Algorithm/Min_Subbarrya_sum.py
@@ -0,0 +1,11 @@
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+def minSubArray(self, nums):
+    best=nums[0]
+    ans=nums[0]
+    for i in range(1,len(nums)):
+        v1=best+nums[i]
+        v2=nums[i]
+        best=min(v1,v2)
+        ans=min(best,ans)
+    return ans
+print(minSubArray(0, nums))
