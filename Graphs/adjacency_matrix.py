a820e22471f5506fb3a391a72293090d64d495b2
Aakash Sharma <11128rpvvgp@gmail.com>
2026-08-09 15:53:12 +0530

    Adjacency Matrix Representation of Graph

--- Graphs/adjacency_matrix.py
+++ Graphs/adjacency_matrix.py
@@ -1,51 +1,28 @@
 class Graph:
-    def __init__(self, vertices: int) -> None:
+    def __init__(self, vertices):
+        self.matrix = [[0] * vertices for _ in range(vertices)]
         self.size = vertices
-        self.mat = [[0] * vertices for _ in range(vertices)]
 
-    def add_edge_undirected(self, src: int, dest: int) -> None:
-        """
-        Adds an undirected edge between src and dest.
-        """
+    def add_edge(self, src, dest):
         if 0 <= src < self.size and 0 <= dest < self.size:
-            self.mat[src][dest] = 1
-            self.mat[dest][src] = 1
+            self.matrix[src][dest] = 1
+            self.matrix[dest][src] = 1   # undirected graph
         else:
-            print("Invalid Edge")
+            print("Invalid edge")
 
-    def add_edge_directed(self, src: int, dest: int) -> None:
-        """
-        Adds a directed edge from src to dest.
-        """
-        if 0 <= src < self.size and 0 <= dest < self.size:
-            self.mat[src][dest] = 1
-        else:
-            print("Invalid Edge")
-
-    def add_edge_weighted(self, src: int, dest: int, weight: int) -> None:
-        """
-        Adds a weighted edge from src to dest with the given weight.
-        """
-        if 0 <= src < self.size and 0 <= dest < self.size:
-            self.mat[src][dest] = weight
-        else:
-            print("Invalid Edge")
-
-    def print_matrix(self) -> None:
-        """
-        Prints the adjacency matrix.
-        """
-        for row in self.mat:
+    def print_matrix(self):
+        for row in self.matrix:
             print(" ".join(map(str, row)))
 
 
-if __name__ == '__main__':
-    g = Graph(5)
-    g.add_edge_undirected(0, 1)
-    g.add_edge_undirected(0, 2)
-    g.add_edge_undirected(1, 3)
-    g.add_edge_undirected(2, 3)
-    g.add_edge_undirected(3, 4)
-    
-    print("Graph Adjacency Matrix:")
-    g.print_matrix()
+a = Graph(5)
+a.add_edge(0, 1)
+a.add_edge(0, 4)
+a.add_edge(1, 2)
+a.add_edge(1, 3)
+a.add_edge(1, 4)
+a.add_edge(2, 3)
+a.add_edge(3, 4)
+
+print("Adjacency Matrix:")
+a.print_matrix()
