1060a854f82e8c821df49c1d6f6adba89e1d4879
Aakash Sharma <11128rpvvgp@gmail.com>
2026-08-11 17:06:13 +0530

    Insertion in binary Tree

---
+++ Trees/BST/Insert into BST.py
@@ -0,0 +1,21 @@
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        if root is None:
+            return TreeNode(val)
+        if root.val>val:
+            root.left= self.insertIntoBST(root.left,val)
+        elif  root.val<val:
+            root.right= self.insertIntoBST(root.right,val)
+        return root
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)    
+print(Solution().insertIntoBST(root, 3).left.right.val)  # Output: 3
