15c7cbace6576df8e39cad4c20fd5842bce61f0d
Aakash Sharma <11128rpvvgp@gmail.com>
2026-09-06 19:44:01 +0530

    Min number of Arrows

---
+++ Merge Interval/Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,17 @@
+points = [[10,16],[2,8],[1,6],[7,12]]
+class Solution(object):
+    def findMinArrowShots(self, points):
+        points.sort()
+
+        arrows = 1
+        end = points[0][1]
+
+        for i in range(1, len(points)):
+            if points[i][0] > end:
+                arrows += 1
+                end = points[i][1]
+            else:
+                end = min(end, points[i][1])
+
+        return arrows
+print(Solution().findMinArrowShots(points))
