19698939a1ebdd10210b2d0f6c2cd3dc770a679e
Aakash Sharma <11128rpvvgp@gmail.com>
2026-09-24 09:42:03 +0530

    four sum problem

---
+++ Two Pointer/4sum.py
@@ -0,0 +1,52 @@
+nums = [1,0,-1,0,-2,2]
+target = 0
+def fourSum(self, nums, target):
+        nums.sort()
+        result = []
+
+        for i in range(len(nums) - 3):
+
+            # Skip duplicate i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            for j in range(i + 1, len(nums) - 2):
+
+                # Skip duplicate j
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = len(nums) - 1
+
+                while left < right:
+
+                    total = nums[i] + nums[j] + nums[left] + nums[right]
+
+                    if total == target:
+                        result.append([
+                            nums[i],
+                            nums[j],
+                            nums[left],
+                            nums[right]
+                        ])
+
+                        left += 1
+                        right -= 1
+
+                        # Skip duplicate left
+                        while left < right and nums[left] == nums[left - 1]:
+                            left += 1
+
+                        # Skip duplicate right
+                        while left < right and nums[right] == nums[right + 1]:
+                            right -= 1
+
+                    elif total < target:
+                        left += 1
+
+                    else:
+                        right -= 1
+
+        return result
+print(fourSum(0, nums, target))
